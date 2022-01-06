9ac39da205236cfc3b7959ae6e2bd50968d35157
johnconstantinides <johnconstantinides02@example.com>
2022-01-05 23:56:09 -0500

    connected to data base and able to get data

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const episodeModel = require('./models/episodes')
 
 require('dotenv').config();
 
@@ -7,8 +9,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`server running on port : ${port}`)
 })
+
+//connect to database
+mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser: true});
+
+//notifys when connected to database
+mongoose.connection.once('open', () => {
+    console.log("connected to database")
+})
+
+app.get('/',(req,res) => {
+    episodeModel.find()
+        .then(episodes => res.json(episodes))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
