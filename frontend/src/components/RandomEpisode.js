60fb7915a4227cba597a74eb409196e05130d9f4
johnconstantinides <johnconstantinides02@example.com>
2022-01-07 18:46:48 -0500

    finished basic part of it so far just need to host and gonna add rating system later and episode list

--- frontend/src/components/RandomEpisode.js
+++ frontend/src/components/RandomEpisode.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../index.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function RandomEpisode() {
 
@@ -18,20 +19,40 @@ function RandomEpisode() {
     },[])
 
     return(
-        <div class="randomepisode">
-        <h1>Random Seinfeld Episode</h1>
-        <h2>{data.title}</h2>
-        <figure>
-            <img src ={data.episode_cover} alt="Episode cover" className="responsive" />
-            <figcaption> Season {data.season} Episode {data.episode}</figcaption>
-        </figure>
-        <div class="descripton">
-            <h3>Description</h3>
-            <p >{data.description}</p>
-        <button onClick={getEpisodeData}>Get Another Episode</button>
-        </div>
+        <div class="RandomEpisode">
+            <body>
+            <div class="container">
+                <div class="jumbotron">
+                <h1>Random Seinfeld Episode</h1>
+                </div>
+            </div>
+            <br/>
+                <div class="container" id="content">
+                        <img src={data.episode_cover} class="img-fluid" id="epsiode_cover"/>
+                </div>
+                <br/>
+                <br/>
+                <div class="container">
+
+                    <div class="row">
+                        <div class="alert alert-secondary" role="alert">
+                            <h4 class="alert-heading">Episode title: {data.title}</h4>
+                            <h5 class="alert-heading">Season {data.season} Episode {data.episode}</h5>
+                            <p>{data.description}</p>
+                        </div>
+                        <br/>
+                    </div>
+                    <button type="button" class="btn btn-primary" onClick={getEpisodeData}>Get a new Episode</button>
+                </div>
+                
+            </body>
         </div>
     )
+    //data.title
+    //data.episode_cover
+    //data.season
+    //data.episode
+    //data.description
 }
 
 export default RandomEpisode;
