0d5c50a1837349b724bc28b8d4818029af2fe1fe
johnconstantinides <johnconstantinides02@example.com>
2022-01-08 02:18:53 -0500

    done i think

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "frontend", "build")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-});
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "frontend", "build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    });
+}
 
 app.listen(port, () => {
     console.log(`server running on port : ${port}`)
