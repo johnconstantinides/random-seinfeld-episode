5181e5c1d3e927d02143086738d5084c39a1dfa4
johnconstantinides <johnconstantinides02@example.com>
2022-01-08 01:45:35 -0500

    ok

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "./fronted/build")));
+app.use(express.static(path.join(__dirname, "frontend", "build")));
 
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
-})
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`server running on port : ${port}`)
